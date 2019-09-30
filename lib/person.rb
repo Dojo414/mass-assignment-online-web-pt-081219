0c0f8343f972f285f16515b5c8fef0741ec1b247
Donovan Jones <donovanljones94@gmail.com>
2019-09-30 13:37:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,14 +3,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width)
-@name = name
-@birthday = birthday
-@hair_color = hair_color
-@eye_color = eye_color
-@height = height
-@weight = weight
-@handed = handed
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)
+  end
 end
